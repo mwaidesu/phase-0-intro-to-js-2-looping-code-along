da57913a73a656c1a9fc54d889763e9b2d9a411c
Mwai <machariamwai@gmail.com>
2022-04-10 19:20:15 +0300

    Pass all looping tests

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+
+function writeCards(names, event) {
+  let myArray = []
+  for (let i = 0; i < names.length; i++) {
+    myArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+  }
+
+  console.log(myArray)
+  return myArray;
+}
+
+
+
+
+
+function countDown(int) {
+  let countDown = int;
+  while (countDown >= 0) {
+    console.log(countDown--);
+  }
+}
+
+countDown(10)
